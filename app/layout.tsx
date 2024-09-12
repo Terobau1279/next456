e45c79ff96fd5cf92f691f98317f3f53d7ab1500
Terobau1279 <johnwho864@gmail.com>
2024-09-13 00:59:24 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,26 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={spaceGrotesk.className} suppressHydrationWarning>
       <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+       
         <meta name="theme-color" content="#ffffff" />
       </head>
       <GoogleAnalytics gaId="G-84Z171LN4N" />
